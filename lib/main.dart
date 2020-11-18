372093f4ef603bcd0ccb0b4350ccafbfcc5dd5d1
Keito <54165015+Tanesan@users.noreply.github.com>
2020-11-18 23:36:51 +0900

    firebase set up and output the data of cloudfirestore.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import 'question.dart';
 import 'setting.dart';
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -22,7 +28,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.teal,
           title: Text(
-           'Get the job!',
+            'Get the job!',
             style: TextStyle(
               fontSize: 20,
             ),
@@ -35,7 +41,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      routes: <String, WidgetBuilder> {
+      routes: <String, WidgetBuilder>{
         '/start_page': (BuildContext context) => StartPage(),
         '/edit_page': (BuildContext context) => EditPage(),
         '/setting_page': (BuildContext context) => setting,
@@ -49,30 +55,30 @@ class AppPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              RaisedButton(
-                onPressed: () => Navigator.of(context).pushNamed("/start_page"),
-                child: Text(
-                  'Start',
-                ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              onPressed: () => Navigator.of(context).pushNamed("/start_page"),
+              child: Text(
+                'Start',
               ),
-              RaisedButton(
-                onPressed: () => Navigator.of(context).pushNamed("/edit_page"),
-                child: Text(
-                  'Edit',
-                ),
+            ),
+            RaisedButton(
+              onPressed: () => Navigator.of(context).pushNamed("/edit_page"),
+              child: Text(
+                'Edit',
               ),
-              RaisedButton(
-                textColor: Color(0xFF6200EE),
-                onPressed: ()  => Navigator.of(context).pushNamed("/setting_page"),
-                child: Text("Setting"),
-              )
-            ],
-          ),
+            ),
+            RaisedButton(
+              textColor: Color(0xFF6200EE),
+              onPressed: () => Navigator.of(context).pushNamed("/setting_page"),
+              child: Text("Setting"),
+            )
+          ],
         ),
+      ),
     );
   }
 }
@@ -89,84 +95,112 @@ class _StartPageState extends State<StartPage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.teal,
-        title: Text(
-          'Get the job!',
-          style: TextStyle(
-            fontSize: 20,
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: Text(
+            'Get the job!',
+            style: TextStyle(
+              fontSize: 20,
+            ),
           ),
         ),
-      ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 100,
-          ),
-          Container(
+        body: Container(
             decoration: BoxDecoration(
               border: Border.all(color: Colors.black54, width: 2),
               borderRadius: BorderRadius.circular(8),
             ),
-            child: Padding(
+            child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('questions')
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  return (!snapshot.hasData)
+                      ? Container()
+                      : ListView.builder(
+                          itemCount: snapshot.data.docs.length,
+                          itemBuilder: (context, index) {
+                            return Column(children: [
+                              QuestionCard(
+                                problemInt: index + 1.toInt(),
+                                questionText: snapshot.data.docs[index]
+                                    .get('questionText'),
+                              ),
+                            ]);
+                          },
+                        );
+                })));
+  }
+}
+
+class QuestionCard extends StatelessWidget {
+  final int problemInt;
+  final String questionText;
+
+  const QuestionCard({
+    Key key,
+    @required this.problemInt,
+    @required this.questionText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      SizedBox(
+        height: 100,
+      ),
+      Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black54, width: 2),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Padding(
               padding: const EdgeInsets.all(10.0),
               child: Column(
                 children: [
+                  Text("${problemInt}"),
                   Text(
-                      "${quizBrain.answeredQuestion}/${quizBrain.numberOfQuestions}"
-                  ),
-                  Text(
-                    "${quizBrain.getQuestionText()}",
-                    style: TextStyle(
-                        fontSize: 20
-                    ),
+                    "${questionText}",
+                    style: TextStyle(fontSize: 20),
                   ),
                 ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(100.0),
-            child: Container(
-              child: Image.asset('images/image_interviewer.png'),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(32.0),
-            child: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  RaisedButton(
-                    color: Colors.amber,
-                    onPressed: () => Navigator.of(context).pop(),
-                    child: Text('Exit'),
-                  ),
-                  SizedBox(width: 50),
-                  RaisedButton(
-                    color: Colors.green,
-                    // onPressed: () => Navigator.of(context).pop(),
-                    onPressed: () => {
-                      setState((){
-                        quizBrain.nextQuestion();
-                        
-                        if(quizBrain.isFinished()){
-                          Navigator.of(context).pop();
-                        }
-                      }),
-                    },
-                    child: Text('Next'),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+              ))),
+      SizedBox(
+        height: 10,
       ),
-    );
+      Padding(
+        padding: const EdgeInsets.all(100.0),
+        child: Container(
+          child: Image.asset('images/image_interviewer.png'),
+        ),
+      ),
+      Container(
+          padding: EdgeInsets.all(32.0),
+          child: Center(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                RaisedButton(
+                  color: Colors.amber,
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Exit'),
+                ),
+                SizedBox(width: 50),
+                RaisedButton(
+                  color: Colors.green,
+                  // onPressed: () => Navigator.of(context).pop(),
+                  onPressed: () => {
+                    // setState((){
+                    //quizBrain.nextQuestion();
+
+                    // if(quizBrain.isFinished()){
+                    //Navigator.of(context).pop();
+                    // }
+                    // }),
+                  },
+                  child: Text('Next'),
+                )
+              ])))
+    ]);
   }
 }
 

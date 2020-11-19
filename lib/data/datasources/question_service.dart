9205ce81572d1c5f8ce981c4095864262901b246
mox692 <55653825+mox692@users.noreply.github.com>
2020-11-19 13:33:47 +0900

    fix format in question_service.dart

--- lib/data/datasources/question_service.dart
+++ lib/data/datasources/question_service.dart
@@ -2,17 +2,21 @@ import 'package:job_interview_practice/data/models/question.dart';
 import 'package:job_interview_practice/data/repositories/question_repository_impl.dart';
 
 class QuestionService {
-
   QuestionService(this.numberOfQuestions);
 
-  int           numberOfQuestions; // todo: numberofquestionをsettings pageから引っ張ってくる。
-  int           answeredQuestion;
+  int numberOfQuestions; // todo: numberofquestionをsettings pageから引っ張ってくる。
+  int answeredQuestion;
   QuestionModel nowQuestion;
 
-  int    getAnsweredQuestion() {return this.answeredQuestion;}
-  String getQuestionText() {return nowQuestion.questionText;}
+  int getAnsweredQuestion() {
+    return this.answeredQuestion;
+  }
 
-  void   nextQuestion() {
+  String getQuestionText() {
+    return nowQuestion.questionText;
+  }
+
+  void nextQuestion() {
     QuestionRepositoryImpl qr = QuestionRepositoryImpl();
     nowQuestion = qr.selectByID("");
     this.answeredQuestion++;

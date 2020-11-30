a7a52ffa5fba36d6f1696ea2dce07a304d9a3320
mox692 <55653825+mox692@users.noreply.github.com>
2020-11-30 19:32:24 +0900

    insert usecase in bloc

--- lib/feature/start/presentation/bloc/start_next_bloc.dart
+++ lib/feature/start/presentation/bloc/start_next_bloc.dart
@@ -1,16 +1,22 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:job_interview_practice/feature/question/data/models/question.dart';
+import 'package:job_interview_practice/feature/setting/domain/usecases/get_number_of_question_use_case.dart';
 import 'package:job_interview_practice/feature/start/presentation/bloc/start_event.dart';
 import 'package:job_interview_practice/feature/start/presentation/bloc/start_state.dart';
 
 class StartNextBloc extends Bloc<StartEvent, StartState> {
-  StartNextBloc() : super(LoadingStartState()) {
+  StartNextBloc({
+    GetNumberOfQuestionUseCase getNumberOfQuestionUseCase,
+  })  : _getNumberOfQuestionUseCase = getNumberOfQuestionUseCase,
+        _numberOfTotalQuestions =
+            10, // todo: get from local setting [number of quesitons]
+        super(LoadingStartState()) {
     add(SetupStartEvent());
   }
 
+  final GetNumberOfQuestionUseCase _getNumberOfQuestionUseCase;
   int _questionCount = 0;
-  int _numberOfTotalQuestions =
-      10; // todo: get from local setting [number of quesitons]
+  final _numberOfTotalQuestions;
 
   @override
   Stream<StartState> mapEventToState(StartEvent event) async* {

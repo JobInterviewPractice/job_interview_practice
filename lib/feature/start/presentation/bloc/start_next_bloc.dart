1661ed53d71f1664436c8756a724b77dadfddb8a
mox692 <55653825+mox692@users.noreply.github.com>
2020-12-01 14:13:26 +0900

    add question_repository to start_page

--- lib/feature/start/presentation/bloc/start_next_bloc.dart
+++ lib/feature/start/presentation/bloc/start_next_bloc.dart
@@ -1,14 +1,17 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:job_interview_practice/core/logic/usecase.dart';
 import 'package:job_interview_practice/feature/question/data/models/question.dart';
+import 'package:job_interview_practice/feature/question/data/repositories/question_repository_impl.dart';
 import 'package:job_interview_practice/feature/setting/domain/usecases/get_number_of_question_use_case.dart';
 import 'package:job_interview_practice/feature/start/presentation/bloc/start_event.dart';
 import 'package:job_interview_practice/feature/start/presentation/bloc/start_state.dart';
 
 class StartNextBloc extends Bloc<StartEvent, StartState> {
-  StartNextBloc({
-    GetNumberOfQuestionUseCase getNumberOfQuestionUseCase,
-  })  : _getNumberOfQuestionUseCase = getNumberOfQuestionUseCase,
+  StartNextBloc(
+      {GetNumberOfQuestionUseCase getNumberOfQuestionUseCase,
+      QuestionRepositoryImpl questionRepositoryImpl})
+      : _getNumberOfQuestionUseCase = getNumberOfQuestionUseCase,
+        _questionRepositoryImpl = questionRepositoryImpl,
         super(LoadingStartState()) {
     add(NextStartEvent());
     fetchNumberOfTotalQuestions();
@@ -21,24 +24,31 @@ class StartNextBloc extends Bloc<StartEvent, StartState> {
         numberOfTotalQuestionEither.fold((l) => 10, (r) => r.number);
   }
 
+  void fetchQuestions(int n) async {
+    this.questions = await _questionRepositoryImpl.selectByRandom(n);
+  }
+
   final GetNumberOfQuestionUseCase _getNumberOfQuestionUseCase;
+  final QuestionRepositoryImpl _questionRepositoryImpl;
   int _questionCount = 0;
   int _numberOfTotalQuestions;
+  List<QuestionModel> questions;
+  QuestionModel nowQuestion;
 
   @override
   Stream<StartState> mapEventToState(StartEvent event) async* {
     if (event is SetupStartEvent) {
       yield LoadingStartState();
     } else if (event is NextStartEvent) {
-      // todo:fetch next question
-      QuestionModel nextQuestion = QuestionModel("test", "test", false);
-
       _questionCount++;
+
+      this.nowQuestion = questions[_questionCount];
+
       if (_questionCount == _numberOfTotalQuestions) {
         yield FinishQuestionState();
       } else {
         yield LoadedStartState(
-            nowQuestion: nextQuestion.questionText,
+            nowQuestion: nowQuestion.questionText,
             numberOfAnsweringQuestions: _questionCount,
             numberOfTotalQuestions: _numberOfTotalQuestions);
       }

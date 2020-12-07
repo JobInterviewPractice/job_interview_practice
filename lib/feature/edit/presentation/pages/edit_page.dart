55feb9690b63dd6e6215dc8188008ef3e00c1e93
mox692 <55653825+mox692@users.noreply.github.com>
2020-12-08 01:22:56 +0900

    remove unnecessary part

--- lib/feature/edit/presentation/pages/edit_page.dart
+++ lib/feature/edit/presentation/pages/edit_page.dart
@@ -1,11 +1,9 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:job_interview_practice/core/presentation/common_widgets/appBar.dart';
 import 'package:job_interview_practice/dependencies.dart';
 import 'package:job_interview_practice/feature/edit/presentation/bloc/edit_bloc.dart';
 import 'package:job_interview_practice/feature/edit/presentation/bloc/edit_state.dart';
-import 'package:vector_math/vector_math_64.dart' show Vector3;
 
 class EditPage extends StatelessWidget {
   @override
@@ -21,35 +19,8 @@ class EditPage extends StatelessWidget {
 }
 
 class EditWidget extends StatelessWidget {
-  var _cardList = List<PictureCard>();
-
-  void init() {
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-    _cardList.add(PictureCard(questionText: "question 1", isWeak: false));
-  }
-
   @override
   Widget build(BuildContext context) {
-    init();
     return BlocProvider(
       create: (_) => serviceLocator<EditBloc>(),
       child: Container(
@@ -89,11 +60,11 @@ class EditWidget extends StatelessWidget {
 }
 
 class PictureCard extends StatelessWidget {
-  final String questionText;
-  final bool isWeak;
+  String questionText;
+  bool isWeak;
 
-  // todo:こっちをいじる
-  TextEditingController _quoteController = TextEditingController(text: "testt");
+  TextEditingController _quoteController =
+      TextEditingController(text: questionText);
 
   PictureCard({this.questionText, this.isWeak});
 
@@ -114,68 +85,13 @@ class PictureCard extends StatelessWidget {
               leading: Icon(Icons.question_answer_outlined),
               title: TextFormField(
                 controller: _quoteController,
-                decoration: InputDecoration(
-                    // icon: Icon(Icons.person),
-                    hintText: 'Qeustion text here',
-                    labelText: this.questionText),
+                decoration: InputDecoration(labelText: this.questionText),
                 onSaved: (String value) {},
                 validator: (String value) {},
               ),
             ),
             // Image.asset('assets/$_picture'),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class ZoomPage extends StatefulWidget {
-  @override
-  _ZoomPageState createState() => _ZoomPageState();
-}
-
-class _ZoomPageState extends State<ZoomPage> {
-  double _scale = 1.0;
-  double _previousScale = 1.0;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Zoom Page"),
-      ),
-      body: Center(
-        child: GestureDetector(
-          onScaleStart: (ScaleStartDetails details) {
-            print(details);
-            _previousScale = _scale;
-            setState(() {});
-          },
-          onScaleUpdate: (ScaleUpdateDetails details) {
-            print(details);
-            _scale = _previousScale * details.scale;
-            setState(() {});
-          },
-          onScaleEnd: (ScaleEndDetails details) {
-            print(details);
-
-            _previousScale = 1.0;
-            setState(() {});
-          },
-          child: RotatedBox(
-            quarterTurns: 0,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Transform(
-                alignment: FractionalOffset.center,
-                transform: Matrix4.diagonal3(Vector3(_scale, _scale, _scale)),
-                child: CachedNetworkImage(
-                  imageUrl:
-                      "https://images.unsplash.com/photo-1578253809350-d493c964357f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80",
-                ),
-              ),
-            ),
-          ),
         ),
       ),
     );

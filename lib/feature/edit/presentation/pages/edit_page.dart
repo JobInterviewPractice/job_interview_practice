a7e9e6c23c6b36bbff5795b60cac8546c4ddd9b3
Keito <54165015+Tanesan@users.noreply.github.com>
2020-12-11 01:33:39 +0900

    question

--- lib/feature/edit/presentation/pages/edit_page.dart
+++ lib/feature/edit/presentation/pages/edit_page.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:job_interview_practice/core/presentation/common_widgets/appBar.dart';
 
 class EditPage extends StatelessWidget {
+  Map<String, List<String>> _dropDownMenu = {
+    'new': ['IT', 'finance'],
+    'popularity': ['IT', 'finance'],
+  };
+  String _selectedKey;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,6 +16,29 @@ class EditPage extends StatelessWidget {
         child: Center(
           child: Column(
             children: <Widget>[
+              DropdownButton<String>(
+                value: _selectedKey,
+                icon: Icon(Icons.arrow_drop_down),
+                iconSize: 30,
+                elevation: 16,
+                style: TextStyle(fontSize: 20, color: Colors.black),
+                underline: Container(
+                  height: 2,
+                  color: Colors.grey,
+                ),
+                onChanged: (newValue) {
+                  setState(() {
+                    _selectedKey = newValue;
+                  });
+                },
+                items: _dropDownMenu.keys
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
               Text('Edit'),
               RaisedButton(
                 onPressed: () => Navigator.of(context).pop(),
